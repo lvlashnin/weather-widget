3eea93c2f659411146e8226e91b2bc4329e79382
lvlashnin <lvlashnin@gmail.com>
2021-06-10 23:44:11 +0300

    added navigator geolocation

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,8 @@ const wigetParent = document.querySelector('.weather-widget'),
     tempEl = document.querySelector('.weather-widget__temperature'),
     locationEl = document.querySelector('.weather-widget__location'),
     weekdayEl = document.querySelector('.weather-widget__weekday'),
-    dateEl = document.querySelector('.weather-widget__date');
+    dateEl = document.querySelector('.weather-widget__date'),
+    descriptionEl = document.querySelector('.weather-widget__description');
 
 
 
@@ -45,12 +46,23 @@ const changeData = (weather, temperature, location) => {
         }
         if (hours >= 23 || hours <= 6) {
             wigetParent.classList.add('weather-widget--night');
+            descriptionEl.classList.add('weather-widget__description--night')
             wigetIcon.src = 'img/icon-night.png';
 
         }
     }
 };
 
-fetch("https://api.openweathermap.org/data/2.5/weather?id=706483&units=metric&appid=1cd8359be73c9ae2115d2cf2f813cc25")
-    .then(response => response.json())
-    .then(json => changeData(json.weather.main, json.main.temp, json.name));
+const success = (pos) => {
+    const crd = pos.coords;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&units=metric&appid=1cd8359be73c9ae2115d2cf2f813cc25`)
+        .then(response => response.json())
+        .then(json => changeData(json.weather.main, json.main.temp, json.name));
+};
+
+const error = (error) => {
+    console.log(error);
+};
+
+navigator.geolocation.getCurrentPosition(success, error);
+
